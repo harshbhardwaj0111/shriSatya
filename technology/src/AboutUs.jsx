ba62013d4efcf37e7436af416ec150f6d86b3de8
harshbhardwaj0111 <harshbhardwaj0111@gmail.com>
2024-07-13 11:10:25 +0530

    start

---
+++ technology/src/AboutUs.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function AboutUs() 
+{
+  return (
+    <div>
+      <h2 className=' '>About Us</h2>
+    </div>
+  )
+}
+
+export default AboutUs
---
+++ technology/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import pic from './images/contant3.png'
+import AboutUs from './AboutUs'
+function App() {
+  return (
+    <div>
+      <AboutUs />
+    </div>
+  )
+}
+
+export default App
+
